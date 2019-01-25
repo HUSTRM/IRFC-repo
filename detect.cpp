8abf402cda3db93f872ee1838eea1a97c67a9c4f
HamPerdredes <perdredes@outlook.com>
2019-01-25 20:37:27 +0800

    Update detect.cpp

--- detect.cpp
+++ detect.cpp
@@ -63,138 +63,14 @@ int otsu(Mat binary)		//大津法Otsu
 	return thresh;
 }
 
-void multi_seg(Mat src)//先利用Otsu算法将图片分为两类，取类内方差最大的继续使用Otsu算法分开，直到类间方差与图像总方差比值满足一定条件
-{
-	int thresh=0;
-	int histogram[256] = { 0 };
-	int threshs[256] = { 0 };
-	int q, gray_max, gray_min, i, j, size;
-	double gray_ave, vt;
-	int g_sum_min, g_sum_max, p_sum_min, p_sum_max;
-	double var_max = 0;//每一类的类内方差及最大方差
-	double var_all;//类间方差
-	double w0 = 0, w1 = 0; //前景和背景所占整幅图像的比例  
-	double u0 = 0, u1 = 0; //前景和背景的平均灰度
-	double u = 0;
-	double variance = 0;
-	double max_variance = 0;//最大类间方差
-	double SF;
-	gray_ave = 0;//图像的平均灰度值
-	gray_max = 0;
-	gray_min = 255;
-	vt = 0;//vt为图像的总方差
-	SF = 0;//SF为结束条件,SF=omeg/vt
-	q = 1;
-	//cvtColor(src, src, COLOR_BGR2GRAY);
-	Mat img = src.clone();
-	size = (img.cols)*(img.rows);
-	for (i = 0; i < img.rows; i++) //获得灰度直方图
-		for (j = 0; j < img.cols; j++) {
-			int x = img.at<uchar>(i, j);
-			if (x > gray_max)
-				gray_max = x;
-			if (x < gray_min)
-				gray_min = x;
-			histogram[x]++;
-		}
-	threshs[0] = gray_min;
-	threshs[1] = gray_max;
-	for (i = gray_min; i <= gray_max; i++)
-		gray_ave += (histogram[i] * i);
-	gray_ave /= size;
-	for (i = gray_min; i <= gray_max; i++)
-		vt = vt + histogram[i] * (i - gray_ave)*(i - gray_ave);
-	vt /= size;
-	while (SF<0.95)//SF>0.95认为分割较为完全
-	{
-		SF = 0;
-		max_variance = 0;
-		int isize = 0;
-		for (i = gray_min; i <= gray_max; i++)
-			isize += histogram[i];
-		for (i = gray_min; i <= gray_max; i++)
-		{
-			g_sum_max = 0;
-			g_sum_min = 0;
-			p_sum_max = 0;
-			p_sum_min = 0;
-			w0 = 0;
-			w1 = 0;
-			for (j = gray_min; j < i; j++) {
-				g_sum_min += j * histogram[j];
-				p_sum_min += histogram[j];
-			}
-			u0 = (double)g_sum_min / p_sum_min;
-			w0 = (double)p_sum_min / isize;
-			for (j = i; j <= gray_max; j++) {
-				g_sum_max += histogram[j] * j;
-				p_sum_max += histogram[j];
-			}
-			u1 = (double)g_sum_max / p_sum_max;
-			w1 = 1 - w0;
-			u = u0 * w0 + u1 * w1;
-			variance = w0 * w1*(u0 - u1)*(u0 - u1);
-			if (variance > max_variance) {
-				max_variance = variance;
-				thresh = i;
-			}
-		}
-		int x = 0;
-		while (thresh >threshs[x] && x <= q)//直接插入排序
-			x++;
-		for (int y = q; y >= x; y--)
-			threshs[y + 1] = threshs[y];
-		threshs[x] = thresh;
-		q++;
-		var_max = 0;
-		double omeg = 0;
-		for (x = 0; x < q; x++) {//类内方差统计
-			int y;
-			int N = 0;
-			double ave = 0;
-			double var_local = 0;
-			for (y = threshs[x]; y <= threshs[x + 1]; y++) {
-				N += histogram[y];
-				ave += y * histogram[y];
-			}
-			ave /= N;
-			omeg += N * (ave - gray_ave)*(ave - gray_ave);
-			for (y = threshs[x]; y <= threshs[x + 1]; y++) {
-				var_local += (y - ave)*(y - ave)*histogram[y];
-
-			}
-			var_local /= N;
-			if (var_local > var_max) {
-				var_max = var_local;
-				gray_min = threshs[x];
-				gray_max = threshs[x + 1];
-			}
-		}
-		omeg /= size;
-		SF = omeg / vt;
-	}
-	//对图像进行分阈值分割
-	uchar *a, *b;
-	for (i = 0; i < src.rows; i++) {
-		a = src.ptr<uchar>(i);
-		b = img.ptr<uchar>(i);
-		for (j = 0; j < src.cols; j++)
-		{
-			int s = 0;
-			while (a[j] > threshs[s] && s < q)
-				s++;
-			b[j] = threshs[s + 1];
-		}
-	}
-	imshow("multi_threshold", img);
-	//imwrite("task6.jpg", img);
-}
 
 
 vector<Point2f> enemy_Dec::obvious_dec(Mat frame)//detecting enemy
-{
+{//隔离掉mask?
 	Mat I;
 	cvtColor(frame,I,CV_BGR2GRAY);
+	morphologyEx(I,I,MORPH_OPEN,element);//MORPH_OPEN
+	morphologyEx(I,I,MORPH_CLOSE,element);//MORPH_CLOSE or not?
 	Mat planes[] = { Mat_<float>(I), Mat::zeros(I.size(), CV_32F) };
 	Mat complexI;
 	//构造复数双通道矩阵
@@ -288,6 +164,7 @@ vector<Point2f> enemy_Dec::obvious_dec(Mat frame)//detecting enemy
 				}
 			}
 		}
+	cout<<"points detected:"<<k<<endl;
 	//invDFT.convertTo(invDFTcvt, CV_32FC1);  
 	imshow("SpectualResidual", invDFTcvt);
 	//imshow("Original Image", I);
@@ -299,29 +176,36 @@ int enemy_Dec::fir_center_dec(vector<Point2f> points)
 //考虑先采用k-means或其他聚类方法找出目标中心，再利用meanshift或KCF对目标进行动态跟踪
 {
 /*-------------------------------------Using K-means to find out the center-------------------------------------------*/
-/*	//容易受边缘噪点干扰
+	//容易受边缘噪点干扰
 	size_t i,j,k=0;
 	Mat samples(points.size(),1,CV_32FC2);
 	Mat lables;
-	Mat center(1,3,samples.type());//3
+	Mat center(3,1,samples.type());//3
 	for(i=0;i<points.size();i++){
 		samples.at<Point2f>(i)=points[i];
 		}
 	kmeans(samples,3,lables,TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0),2,KMEANS_PP_CENTERS, center);
 	if(!center.empty()){
-		for(i=0;i<1;i++)
+		for(i=0;i<3;i++)
 			centers.push_back(Point(center.at<float>(i*2),center.at<float>(i*2+1)));
-		//is_detected=true;
-		selection=Rect(centers[1].x-50,centers[1].y-50,100,100);
+		for(i=0;i<2;i++){
+			for(j=1;j<3;j++){
+				if(centers[i].x>centers[j].x){
+					Point2f temp=centers[i];
+					centers[i]=centers[j];
+					centers[j]=temp;
+				}
+			}	
+		}		
 		return 1;
-		}
+	}
 	else
-		return 0;*/
+		return 0;
 
 /*---------------------Using meanshift to detect the center-----------------------------------------------------------*/
 /*---------------------Using DBSCAN to detect the center---------------------------------------------*/
-	DBSCAN(points,0.8,50);//cost too much time
-	waitKey(10);
+	/*DBSCAN(points,0.8,50);//cost too much time
+	waitKey(10);*/
 }
 
 
@@ -360,35 +244,122 @@ vector<float> enemy_Dec::angle_get(Mat frame)//基于检测到的中心点反馈
 	return angles;
 }
 		
-void enemy_Dec::init()
+Mat enemy_Dec::initial(Mat frame)
 {
-	//tracker(HOG,FIXDWINDOW,MULTISCALE,LAB);
+//预处理还应考虑到对镜头畸变的校正
+/*---------------------------Based on ORB to detect the central area--------------------------------------------------*/
+/*实际效果不佳，考虑基于ORB检测有针对性地删去红色区域*/
+	Mat yuv;
+	cvtColor(frame,yuv,CV_BGR2YUV);
+	vector<Mat> splited;
+	Mat result;
+	split(yuv,splited);
+	addWeighted(splited[2],1,splited[1],-1,0,result);	//索性直接将全部满足一定条件的非零区域置于mask？
+	//morphologyEx(result,result,MORPH_OPEN,element);//MORPH_OPEN
+	int thres=otsu(result);//
+	threshold(result,result,thres,255,THRESH_BINARY);//
+	Mat c_match=imread("result.jpg");
+	Ptr<ORB> orb=ORB::create();
+	vector<KeyPoint> Key_points1,Key_points2;
+	Mat dst1,dst2;
+	orb->detectAndCompute(result,Mat(),Key_points1,dst1);
+	orb->detectAndCompute(c_match,Mat(),Key_points2,dst2);
+	//Show Keypoints
+	Mat show1,show2;
+	drawKeypoints(result,Key_points1,show1);
+	drawKeypoints(c_match,Key_points2,show2);
+	imshow("show1",show1);
+	//imshow("central match",show2);
+	//Matching
+	vector<DMatch> matches;
+	Ptr<DescriptorMatcher> matcher=DescriptorMatcher::create("BruteForce");
+	matcher->match(dst1,dst2,matches);
+	if(matches.size()<100)
+		return frame;
+//RANSAC
+	vector<Point2f> p1,p2;
+	//vector<DMatch> good_matches;
+	vector<KeyPoint> RAN_KP1,RAN_KP2;
+	for(size_t i=0;i<matches.size();i++){
+		RAN_KP1.push_back(Key_points1[matches[i].queryIdx]);
+		RAN_KP2.push_back(Key_points2[matches[i].trainIdx]);
+	}	
+	for(size_t i=0;i<matches.size();i++){
+		p1.push_back(Point2f(RAN_KP1[i].pt.x,RAN_KP1[i].pt.y));
+		p2.push_back(Point2f(RAN_KP2[i].pt.x,RAN_KP2[i].pt.y));
+	}
+	vector<uchar> RansacStatus;
+	Mat Fundamental=findFundamentalMat(p1,p2,RansacStatus,FM_RANSAC);
+	vector<KeyPoint> RR_KP1,RR_KP2;
+	vector<DMatch> RR_matches;
+	int index=0;
+	for(size_t i=0;i<matches.size();i++){
+		if(RansacStatus[i]!=0){
+			RR_KP1.push_back(RAN_KP1[i]);
+			RR_KP2.push_back(RAN_KP2[i]);
+			matches[i].queryIdx=index;
+			matches[i].trainIdx=index;
+			RR_matches.push_back(matches[i]);
+			index++;
+		}
+	}
+	//show matched pic
+	Mat matched;
+	drawMatches(result,RR_KP1,c_match,RR_KP2,RR_matches,matched);
+	imshow("matched",matched);
+	//test 将所有非零像素区域置为mask进行测试
+	Mat ans=Mat(frame.size(),frame.type(),Scalar(210,210,210));//based on the actual background
+	mask=Mat::zeros(frame.size(),CV_8UC1);
+	for(int i=0;i<result.rows;i++){
+		uchar* p=mask.ptr<uchar>(i);
+		const uchar* q=result.ptr<uchar>(i);
+		for(int j=0;j<result.cols;j++){
+			if(q[j]==0)//black area
+				p[j]=255;
+		}
+	}
+	//imshow("mask",mask);
+	frame.copyTo(ans,mask);
+	return ans;
+	//imshow("result",matched);
+	waitKey(0);
+//poor behavior  //考虑对检测出的红色区域置为全图的均值以减小干扰？!!!
 }
 
 int enemy_Dec::process(Mat frame)
 {
-	if(trackCount<400)//此前未检测到目标
-	{
-		cout<<frame.rows<<endl;
-		cout<<frame.cols<<endl;
+	if(trackCount<6)//检测同一目标的次数小于6次
+	{//能否优化流程以降低算法时间复杂度？
+		//cout<<frame.rows<<endl;
+		//cout<<frame.cols<<endl;
 		clock_t start=clock();
-		vector<Point2f> points=obvious_dec(frame);
-		//trackCount=fir_center_dec(points);
-		centers.clear();
+		vector<Point2f> points=obvious_dec(frame);//显著性区域检测
 		fir_center_dec(points);
-		trackCount++;
+		if(trackCount==0){
+			old_center=centers[1];
+			trackCount++;
+		}
+		else if(fabs(centers[1].x-old_center.x)<15&&fabs(centers[1].y-old_center.y)<15){//same point
+			trackCount++;
+			old_center=centers[1];
+		}
+		else{//detect another point
+			trackCount=0;
+		}
 		//Point center=fir_center_dec(result);
 		for(size_t i=0;i<centers.size();i++){
 			circle(frame,centers[i],20,Scalar(0,0,255));
 			}
+		centers.clear();
 		imshow("centers",frame);
 		clock_t end=clock();
 		float t=end-start;
 		t/=1000000;
 		cout<<t<<endl;
-		waitKey(1);
+		waitKey(10);
 	}
-	else{
+	else{				//引入追踪过程中的纠错机制，除了结合传感器检测，算法上如何自行判断是否检测到了
+					//从KCF算法本身入手
 //此前已检测到目标//考虑改用KCF
 /*-------------------------using meanshift to track--------------------------------------------------------------*/
 /*		int smin=30,vmin=10,vmax=256;//adjustable!!!
@@ -420,7 +391,8 @@ int enemy_Dec::process(Mat frame)
 		waitKey(100);*/
 /*-----------------------Using KCF to track----------------------------------------------------------------------------*/
 		
-		if(trackCount==400){
+		if(trackCount==6){
+			selection=Rect(old_center.x-50,old_center.y-50,100,100);
 			tracker.init(selection,frame);
 			rectangle(frame,Point(selection.x,selection.y),Point(selection.x+selection.width,selection.y+selection.height),Scalar(0,0,255),1,CV_AA);
 			}
@@ -431,7 +403,7 @@ int enemy_Dec::process(Mat frame)
 		if(!SILENT){
 			imshow("tracing",frame);
 			trackCount++;
-			waitKey(5);
+			waitKey(50);
 		}
 /*---------------------------------------------------------------------------------------------------------------------*/
 	}
@@ -445,8 +417,16 @@ int main()
 	cap.open("test.mp4");
 	enemy_Dec test;
 	//test.init();
+	int f=0;
 	while(1){
 		cap>>frame;
+		if(f<20){
+			f++;
+			continue;
+		}
+			
+		//Mat ans=test.initial(frame);
+		//test.process(ans);
 		test.process(frame);
 	}	
 	return 0;
