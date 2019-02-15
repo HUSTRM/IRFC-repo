2053820ed3212adf6853851fa64ee568c5794637
HamPerdredes <perdredes@outlook.com>
2019-02-15 14:05:38 +0800

    Create head.h

---
+++ head.h
@@ -0,0 +1,29 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <features2d.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <iostream>
+#include <assert.h>
+#include <cstdlib>
+#include <time.h>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <iosfwd>
+#include <memory>
+#include <utility>
+#include <fstream>
+#include <sstream>
+
+
+
+#endif
+
